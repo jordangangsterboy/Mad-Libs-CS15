3174636182a090360e60f3b7a516ec3670ba9fd1
jordangangsterboy <jordantroar@gmail.com>
2026-09-08 14:55:47 -0600

    class 2

---
+++ yessir.py
@@ -0,0 +1,9 @@
+noun = input("Noun\n")
+adjective = input("Adjective\n")
+verb = input("Verb ending in -ing\n")
+name = input("Student name\n")
+number = input("Number\n")
+plural_noun = input("Plural noun\n")
+adjective_2 = input("Another adjective\n")
+
+print(f"{noun} balal")
